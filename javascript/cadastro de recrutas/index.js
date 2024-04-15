312336e70e13a7de4e4a7b4a37197e7cbfe9e619
André Nascimento <162403231+Profissionalandre@users.noreply.github.com>
2024-04-15 18:47:14 -0300

    adiçao da pasta recruta

---
+++ javascript/cadastro de recrutas/index.js
@@ -0,0 +1,13 @@
+const primeiroNome = prompt("Qual seu primeiro nome?");
+const sobrenome = prompt("Qual seu sobrenome?");
+
+const estudo = prompt("Qual seu campo de estudos?");
+const data = prompt("Data de Nascimento:");
+
+document.write("Nome: " + primeiroNome + " " + sobrenome);
+document.write("<br> Estudo: " +estudo);
+document.write("<br> Idade: " +  (2024 - parseFloat(data)) );
+qjn-vuxi-pro
+
+
+
