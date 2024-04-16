b9f4c3310cd62e5b9b95e1115a8ccbeaa541b549
André Nascimento <162403231+Profissionalandre@users.noreply.github.com>
2024-04-16 11:56:18 -0300

    seguindo o exercicio

--- javascript/cadastro de recrutas/index.js
+++ javascript/cadastro de recrutas/index.js
@@ -4,9 +4,9 @@ const sobrenome = prompt("Qual seu sobrenome?");
 const estudo = prompt("Qual seu campo de estudos?");
 const data = prompt("Data de Nascimento:");
 
-document.write("Nome: " + primeiroNome + " " + sobrenome +
-"<br> Estudo: " + estudo +
-"<br> Idade: " +  (2024 - parseFloat(data)) );
+alert("Nome: " + primeiroNome + " " + sobrenome + 
+"\nEstudo: " + estudo + 
+"\nIdade: " + (2024 - parseFloat(data)) );
 
 
 
