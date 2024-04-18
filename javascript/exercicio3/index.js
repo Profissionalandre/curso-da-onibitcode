3d8f9aa13869af8d0f0800668af91e4345206599
André Nascimento <162403231+Profissionalandre@users.noreply.github.com>
2024-04-18 09:02:54 -0300

    Index minha forma, conversor forma do professor

--- javascript/exercicio3/index.js
+++ javascript/exercicio3/index.js
@@ -1,25 +1,28 @@
-let number = parseFloat(prompt("Digite um valor em Metros:"))
-let medida = parseFloat(prompt("Escolha um valor das medidas que quer converter:\n1. Milímetros (mm):\n2. Centímetros (cm):\n3. Decímetros (dm):\n4. Decâmetros (dam):\n5. Hectômetros (hm):\n6. Quilômetros (km):\n"))
+// Obter o valor de medida a ser convertido
+let number = parseFloat(prompt("Insira uma medida rm Metros:"))
+// Pedir ao usuário para escolher a unidade para conversão
+let medida = parseFloat(prompt(
+    "Escolha para qual medida que quer converter:\n1. Milímetros (mm):\n2. Centímetros (cm):\n3. Decímetros (dm):\n4. Decâmetros (dam):\n5. Hectômetros (hm):\n6. Quilômetros (km):\n"))
 
-
+// Usra o switch para mostrar os resultados de acordo com as diferentes opções
 switch (medida) {
     case 1:
-        alert(number * 1000 + " " + "mm")
+        alert("Resultado: " + number + "m = " + number * 1000 + " " + "mm")
         break;
     case 2:
-        alert(number * 100 + " " + "cm")
+        alert("Resultado: " + number + "m = " + number * 100 + " " + "cm")
         break;
     case 3:
-        alert(number * 10 + " " + "dm")
+        alert("Resultado: " + number + "m = " + number * 10 + " " + "dm")
         break;
     case 4:
-        alert(number / 10 + " " + "dam")
+        alert("Resultado: " + number + "m = " + number / 10 + " " + "dam")
         break;
     case 5:
-        alert(number / 100 + " " + "hm")
+        alert("Resultado: " + number + "m = " + number / 100 + " " + "hm")
         break;
-        case 6:
-        alert(number / 1000 + " " + "km")
+    case 6:
+        alert("Resultado: " + number + "m = " + number / 1000 + " " + "km")
         break;
     default:
         alert("Opção inválida!")
